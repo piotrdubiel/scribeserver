470f62ee63502dc19a9685c843d6a1ccf5377e29
Piotrek Dubiel <piotr.dubiel@polidea.com>
2014-02-09 21:47:45 +0100

    [Insert your commit message here. Be sure to make it descriptive.]

--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, jsonify, g, request, render_template, redirect, url_for
 from flask.ext.login import current_user
+from flask.ext.security import RegisterForm
+from flask.ext.security.registerable import register_user
+from flask.ext.security.utils import login_user
+from werkzeug.datastructures import MultiDict
 from .decorators import view_authorize, api_authorize, http_authorize
 
 blueprint = Blueprint('users', __name__)
@@ -20,4 +24,12 @@ def home():
 @blueprint.route('/api/token', methods=['POST'])
 @http_authorize
 def token():
+    return current_user.get_auth_token()
+
+@blueprint.route('/api/register', methods=['POST'])
+def register():
+    form_data = MultiDict(request.json)
+    form = RegisterForm(form_data)
+    user = register_user(**form.to_dict())
+    login_user(user)
     return current_user.get_auth_token()
