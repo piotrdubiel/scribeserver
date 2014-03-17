ffdd1b39c62a641f2aa2497994d75580875c01f1
Piotrek Dubiel <piotr.dubiel@polidea.com>
2014-03-17 19:02:49 +0100

    lol digg

--- recognition/views.py
+++ recognition/views.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request
 from tempfile import TemporaryFile
 from users import api_authorize
 from .models import Prediction
-from PIL import Image
 
 blueprint = Blueprint('recognition', __name__)
 
@@ -17,7 +16,7 @@ import pysbp
 # ===============================================
 
 
-@blueprint.route('/api/recognize', methods=['POST'])
+@blueprint.route('/api/image/recognize', methods=['POST'])
 @api_authorize
 def recognize():
     print request.json
@@ -32,9 +31,17 @@ def recognize():
 
     return 'a'
 
+@blueprint.route('/api/pca/recognize', methods=['POST'])
+@api_authorize
+def recognize():
+    print request.json
+    vector = request.json['data'].decode('base64')
+    print vector
+    return 'a'
 
-@blueprint.route('/api/sbp', methods=['GET'])
-def sbp():
+
+@blueprint.route('/api/xor', methods=['GET'])
+def xor():
     pysbp.init()
     pysbp.add_layer([43.259, 43.268, -66.366], [65.075, 65.101, -27.079])
     pysbp.add_layer([-29.041, 27.2972, -5.0622])
